31c632f322a805d111996e6606ce043b4817abb2
Yunarta Kartawahyudi <yunarta@vcube.co.jp>
2018-03-06 16:48:46 +0800

    Rename view controller into SingleTypeTableViewController

---
+++ Example/SwiftVIPERDemo/SingleTypeTableViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  SwiftVIPERDemo
+//
+//  Created by Yunarta Kartawahyudi on 28/2/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import SwiftVIPER
+
+class StringData: VIPERTableDataSource, SingleType {
+
+    func creationInfo(table: VIPERTable) -> CellCreationInfo {
+        return CellCreationInfo(id: 0, nib: "Text")
+    }
+
+    func table(table: VIPERTable, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func present(table: VIPERTable, view: TextCell, at indexPath: IndexPath) {
+        view.present(table: table, data: "data \(indexPath.row)")
+    }
+}
+
+class TextTableView: VIPERViewInterface {
+    
+}
+
+class TextTableViewBinding: VIPERTableViewBinding, VIPERViewBindingInterface {
+    
+    let tableData = VIPERTableData<StringData>(dataSource: StringData())
+    
+    var view = TextTableView()
+    
+    @IBOutlet weak var tableView: UITableView?
+    
+    required init() {
+        super.init(view: view, tableData: tableData)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let tableView = tableView else {
+            return
+        }
+        
+        tableData.install(to: tableView)
+    }
+}
+
+class SingleTypeTableViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        dispatchViewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
--- Example/SwiftVIPERDemo/TextCell.swift
+++ Example/SwiftVIPERDemo/TextCell.swift
@@ -1,12 +1,12 @@
 //
-//  ViewController.swift
+//  TextCell.swift
 //  SwiftVIPERDemo
 //
-//  Created by Yunarta Kartawahyudi on 28/2/18.
+//  Created by Yunarta Kartawahyudi on 6/3/18.
 //  Copyright © 2018 CocoaPods. All rights reserved.
 //
 
-import UIKit
+import Foundation
 import SwiftVIPER
 
 protocol TextCellView: VIPERViewInterface {
@@ -49,7 +49,7 @@ class TextCellViewBinding: UIView, VIPERCellView {
 }
 
 class TextCellPresenter: VIPERCellPresenter {
-     
+    
     func present(table: VIPERTable, view: TextCellView, data: String) {
         view.text = data
     }
@@ -57,74 +57,18 @@ class TextCellPresenter: VIPERCellPresenter {
 
 
 class TextCell: UITableViewCell, VIPERTableCellView {
- 
+    
     var presenter: TextCellPresenter? = TextCellPresenter()
-
+    
     var layoutMode: VIPERTableCellViewLayoutMode = .autoLayout
-
+    
     var layoutView: UIView?
-
+    
     var delegate: TextCellPresenter?
-
+    
     var cellView: TextCellViewBinding {
         return binding
     }
-
+    
     @IBOutlet weak var binding: TextCellViewBinding!
 }
-
-class StringData: VIPERTableDataSource, SingleType {
-
-    func creationInfo(table: VIPERTable) -> CellCreationInfo {
-        return CellCreationInfo(id: 0, nib: "Text")
-    }
-
-    func table(table: VIPERTable, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
-
-    func present(table: VIPERTable, view: TextCell, at indexPath: IndexPath) {
-        view.present(table: table, data: "data \(indexPath.row)")
-    }
-}
-
-class TextTableView: VIPERViewInterface {
-    
-}
-
-class TextTableViewBinding: VIPERTableViewBinding, VIPERViewBindingInterface {
-    
-    let tableData = VIPERTableData<StringData>(dataSource: StringData())
-    
-    var view = TextTableView()
-    
-    @IBOutlet weak var tableView: UITableView?
-    
-    required init() {
-        super.init(view: view, tableData: tableData)
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        guard let tableView = tableView else {
-            return
-        }
-        
-        tableData.install(to: tableView)
-    }
-}
-
-class ViewController: UIViewController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        dispatchViewDidLoad()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-}
-
